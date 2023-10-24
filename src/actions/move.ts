1b2d263debae63b66305a859f6750647b2d38c2f
taolu <zhanglong12138@outlook.com>
2023-10-24 18:06:20 +0800

    add move actions check

--- src/actions/move.ts
+++ src/actions/move.ts
@@ -10,11 +10,21 @@ export default (data:any, socket:any) => {
         }
     }
     if(direction=='row'){
-        console.log(offset)
-        userInfo.position[0] += offset;
+        let positionx = userInfo.position[0] + offset;
+        if(positionx-0.5<0 || positionx + 0.5>CONFIG.stage.width){
+            console.log('not in the stage')
+            return
+        }
+        userInfo.position[0] = positionx;
     }
     if(direction=='column'){
-        userInfo.position[1] += offset;
+        let positiony = userInfo.position[1] + offset;
+        if(positiony-0.5<0 || positiony + 0.5>CONFIG.stage.height){
+            console.log('not in the stage')
+            return
+        }
+        userInfo.position[1] = positiony;
     }
+
     user.set(socket.id, userInfo);
 }
